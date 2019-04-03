072bda3b6f16cfef5a8e3db4840753d26ab61e0d
zhangsan <zhangsan@itcast.com>
2019-04-03 10:17:30 +0800

    修改test

--- src/main/java/com/itheima/git/FirstClass.java
+++ src/main/java/com/itheima/git/FirstClass.java
@@ -8,6 +8,12 @@ public class FirstClass {
         System.out.println("test");
         System.out.println("test");
         System.out.println("test");
+        //hello test
 
+    }
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(1);
     }
 }
